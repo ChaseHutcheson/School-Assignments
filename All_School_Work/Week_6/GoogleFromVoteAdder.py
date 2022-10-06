2c0a40e76f1909cc7ab3ce06a12a24362426d771
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2022-10-06 12:23:21 -0400

    Added Vote Adder

---
+++ All_School_Work/Week_6/GoogleFromVoteAdder.py
@@ -0,0 +1,24 @@
+from msilib.schema import CheckBox
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+def submitform1():
+    web = webdriver.Chrome(service=Service(ChromeDriverManager().install())) # opens google.
+    web.get('https://docs.google.com/forms/d/e/1FAIpQLScXNbdAs0HF9d5DHFXo1qTjK9wMSrG1nBCb1lzFw0tkJUGvOw/viewform')
+
+    text_area1 = web.find_element(By.XPATH, "/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    text_area2 = web.find_element(By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    CheckBox1 = web.find_element(By.XPATH, "/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/span/div/div[5]/label/div/div[1]/div")
+    submit = web.find_element(By.XPATH, "/html/body/div[1]/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div")
+
+    text_area1.send_keys("hutcheson_chase@student.mahoningctc.com")
+    text_area2.send_keys("Hutcheson Chase")
+    CheckBox1.click()
+    submit.click()
+
+
+for i in range(1000):
+    submitform1()
---
+++ I Count Money/Paycheck.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+url = "https://www.google.com/maps/search/used+cars/@41.0216561,-80.7768606,15z/data=!4m2!2m1!6e6"
