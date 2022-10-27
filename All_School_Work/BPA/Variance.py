173a503138f2a6cc5e919b5aab948f748b35d139
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2022-10-27 09:26:13 -0400

    REGHBHFERGFR

--- All_School_Work/BPA/Variance.py
+++ All_School_Work/BPA/Variance.py
@@ -8,6 +8,24 @@ from unittest import result
 dir_path = 'All_School_Work\\BPA\\State_Test'
 count = 1
 
+
+def Print():
+        print(f"""
+        Grill {Calculate()[2]}
+        Min: {Calculate()[3]}
+        Max: {Calculate()[4]}
+        Mean {Calculate()[5]}
+        Standard Deveiation of temps is {Calculate()[6]}
+        {Calculate()[1]}{Calculate()[0]}
+        """)
+        if os.path.isfile(os.path.join(dir_path, path)):
+            global count
+            if count == 5:
+                count = 5
+            else:
+                count += 1
+
+
 for path in os.listdir(dir_path):
     def Calculate():
         #Define Variables
@@ -46,39 +64,23 @@ for path in os.listdir(dir_path):
         grillname = grillFile.replace('All_School_Work\\BPA\\State_Test\\Grill_', '')
         grillname = grillname.replace('.txt', '')
         grillFile = grillFile.replace("1", str(int(grillname) + 1))
-
-        for i in range(len(grillTemps)):
-            if (int(grillTemps[i]) - int(tempMean) < int(deviation) * 2):
-                result = "Fail! "
-                whyResult = "Too Hot: "
-            elif (int(grillTemps[i]) - int(tempMean) > int(deviation) * 2):
-                result = "Fail! "
-                whyResult = "Too Cold: "
-            elif (int(grillTemps[i]) - int(tempMean) > (deviation) * 2) and (int(grillTemps[i]) - int(tempMean) < int(deviation) * 2):
-                result = "Fail! "
-                whyResult = "Too Hot and Too Cold: "
-            if  (int(grillTemps[i]) - int(tempMean) < (deviation) * 2) and (int(grillTemps[i]) - int(tempMean) > int(deviation) * 2):
-                result = "Pass! "
-                whyResult = ""
-
         
+
+        result = ""
+        whyResult = ""
+        for i in range(len(grillTemps)):
+            calc = int(grillTemps[i]) - int(tempMean) <= int(deviation) * 2
+
+            if (calc - 2) <= (deviation * 2) and (calc + 2) >= (deviation * 2):
+                result = "Pass!"
+                whyResult = ""
+            elif (calc - 2) > (deviation * 2):
+                result = "Fail!"
+                whyResult = "Too Hot "
+            elif (calc - 2) < (deviation * 2):
+                result = "Fail!"
+                whyResult = "Too Cold "
+
         return result, whyResult, grillname, lowestTemp, highestTemp, tempMean, deviation, grillTemps
-
-    def Print():
-        print(f"""
-        Grill {Calculate()[2]}
-        Min: {Calculate()[3]}
-        Max: {Calculate()[4]}
-        Mean {Calculate()[5]}
-        Standard Deveiation of temps is {Calculate()[6]}
-        {Calculate()[1]}{Calculate()[0]}
-        """)
-
-        if os.path.isfile(os.path.join(dir_path, path)):
-            global count
-            if count == 5:
-                count = 5
-            else:
-                count += 1
 
     Print()
