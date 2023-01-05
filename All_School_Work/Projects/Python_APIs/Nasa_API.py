7a0fbecf4fbd33a1391b39e0d0a5baa10e2788dd
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-01-05 08:21:50 -0500

    Data structured for NASA API/created Google_API.py

--- All_School_Work/Projects/Python_APIs/Nasa_API.py
+++ All_School_Work/Projects/Python_APIs/Nasa_API.py
@@ -4,4 +4,23 @@ import requests
 response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=4000&camera=pancam&api_key=ojrrlXJErN3MvJrx6ddCMuLFPuKQfyGFUyuuYlbp")
 request = response.request
 
-print(response.json())
+nasaAPI = response.json()
+nasaPhotos = nasaAPI["photos"]
+
+images = []
+imageDates = []
+
+for i in range(len(nasaPhotos)):
+    data = nasaPhotos[i]
+    images.append(data["img_src"])
+    imageDates.append(data["earth_date"])
+
+combinedData = []
+
+for i in range(len(nasaPhotos)):
+    combinedData.append({
+        "imgLink" : images[i],
+        "imgDate" : imageDates[i] 
+    })
+
+print(combinedData)
