e6a224f7f5a30745d323207363af9855bd6529e4
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-01-12 11:49:32 -0500

    Issue with cv2

--- All_School_Work/Projects/Capstone Project/App.py
+++ All_School_Work/Projects/Capstone Project/App.py
@@ -1,11 +1,22 @@
-from kivy.app import App
-from kivy.uix.button import Button
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton
+from kivy.uix import image
+import 
 
-class MainApp(App):
+class MainApp(MDApp):
+
     def build(self):
-        button = Button(text='Hello World', size_hint=(.5, .5), pos_hint={'center_x': .5, 'center_y': .5})
-        return button
+        layout = MDBoxLayout(orientation = 'vertical')
+        self.image = image()
+        layout.add_widget(self.image)
+        layout.add_widget(MDRaisedButton(
+            text='Take A Picture',
+            pos_hint={'center_x': .5, 'center_y': .5},
+            size_hint = (None, None))
+        )
+        self.capture = cv2
+        return layout
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    MainApp().run()
