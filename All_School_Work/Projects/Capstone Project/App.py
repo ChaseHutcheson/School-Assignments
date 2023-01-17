19663b4fde00893296dbb710242671a3c81d00d1
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-01-17 12:45:17 -0500

    Pytesseract Isnt Working

--- All_School_Work/Projects/Capstone Project/App.py
+++ All_School_Work/Projects/Capstone Project/App.py
@@ -1,31 +1,34 @@
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.button import MDRaisedButton
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
 import cv2
 import datetime
-import time
+import pytesseract as pyt
+
 
 class MainApp(MDApp):
 
     def build(self):
-        self.layout = MDBoxLayout(orientation = 'vertical')
+        self.layout = MDFloatLayout()
         self.image = Image()
         self.layout.add_widget(self.image)
         self.save_image_button = MDRaisedButton(
             text='Translate',
-            pos_hint={'center_x': .40, 'center_y': .5},
+            pos_hint={'center_x': .25, 'center_y': .05},
             size_hint = (None, None))
         self.save_image_button.bind(on_press=self.take_picture)
         self.new_image_button = MDRaisedButton(
             text = 'Back',
-            pos_hint={'center_x': .80, 'center_y': .5},
+            pos_hint={'center_x': .75, 'center_y': .05},
             size_hint=(None, None))
-        self.new_image_button.bind(on_press=self.load_video)
+        self.new_image_button.bind(on_press=self.new_picture)
         self.layout.add_widget(self.save_image_button)
         self.layout.add_widget(self.new_image_button)
+        self.new_image_button.disabled = True
         self.capture = cv2.VideoCapture(0)
         self.camera = Clock.schedule_interval(self.load_video, 1.0/30.0)
         self.camera
@@ -43,13 +46,20 @@ class MainApp(MDApp):
         image_name = datetime.datetime.now().strftime('%m-%d-%y, %H;%M;%S') + ".jpg"
         save_path = f"Images\\{image_name}"
         cv2.imwrite(save_path, self.image_frame)
-        self.display_images = Image(source=f"Images\\{image_name}")  
-        self.layout.add_widget(self.display_images)       
+        self.display_images = Image(source=f"Images\\{image_name}")
+        self.layout.add_widget(self.display_images)
+        self.save_image_button.disabled = True
+        self.new_image_button.disabled = False
+        translate_img = cv2.imread(self.display_images)
+        pyt.pytesseract.tesseract_cmd = "C:\\Users\\hutcheson_chase\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+        translated_text = pyt.image_to_string(translate_img)
+        text_label = Label(text = translated_text)
+        self.layout.add_widget(text_label)
 
     def new_picture(self, *args):
-        self.layout.remove_widget(Image())
-        Clock.schedule_interval(self.load_video, 1.0/30.0)
-
+        self.layout.remove_widget(self.display_images)
+        self.save_image_button.disabled = False
+        self.new_image_button.disabled = True
 
 if __name__ == '__main__':
     MainApp().run()
